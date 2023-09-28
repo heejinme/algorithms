51e57a426e14cdaf2fc869062c92d8fc3a0f9b93
heejinme <heejinna.dev@gmail.com>
2023-09-28 10:05:38 +0900

    feat: solve minimum of three values (#1)

---
+++ inflearn/section-1/minimum-of-three.js
@@ -0,0 +1,11 @@
+/**
+* 100이하의 자연수 A, B, C를 입력받아 세 수 중 가장 작은 값을 출력하는 프로그램을 작성하세요.(정렬을 사용하면 안됩니다)
+*/
+
+function solution(a, b, c) {
+  return Math.min(a, b, c);
+}
+
+console.log(solution(6,5,11));
+
+
